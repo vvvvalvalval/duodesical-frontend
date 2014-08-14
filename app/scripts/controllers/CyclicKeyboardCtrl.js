7c392a582cd521d251fc24fdff46ecff06002c17
Valentin Waeselynck <val@bandsquare.fr>
2014-08-14 15:55:48 +0200

    round wrapper

--- app/scripts/controllers/CyclicKeyboardCtrl.js
+++ app/scripts/controllers/CyclicKeyboardCtrl.js
@@ -1,50 +1,68 @@
 angular.module('duodesicalApp')
-  .controller('CyclicKeyboardCtrl',['instruPlayer', '$scope', '$log', 'd12allPitches',
+  .controller('CyclicKeyboardCtrl', ['instruPlayer', '$scope', '$log', 'd12allPitches',
     function (instruPlayer, $scope, $log, d12allPitches) {
 
-    $scope.pitches = d12allPitches;
+      $scope.pitches = d12allPitches;
 
-    // display
-    $scope.ckDisplaySettings = {
-      width: 450,
-      a: 0.00005,
-      octaveHeight: 35,
-      rMin: 30,
-      minPitch: 30,
-      numberDisplayed: 4*12
-    };
-    var ck = $scope.ckDisplaySettings;
-
-    function cartesianCoordinates (r, theta) {
-      var centerX = ck.width/ 2, centerY = ck.width/2;
-      return {
-        x: (centerX + r * Math.cos(theta)),
-        y: (centerY + r * Math.sin(theta))
+      // display
+      $scope.ckDisplaySettings = {
+        width: 450,
+        a: 0.00005,
+        octaveHeight: 0.08,
+        rMin: 0.18,
+        minPitch: 36
       };
-    }
+      var ck = $scope.ckDisplaySettings;
 
-    var twelveth = 2 * Math.PI / 12;
+      function cartesianCoordinates(r, theta) {
+        var centerX = 0.5, centerY = 0.5;
+        return {
+          x: (centerX + r * Math.cos(theta)) * 100,
+          y: (centerY + r * Math.sin(theta)) * 100
+        };
+      }
 
-    function display(pitch){
-      return (pitch >= +ck.minPitch && (pitch - +ck.minPitch) < +ck.numberDisplayed);
-    }
-    $scope.display = display;
-    function getPosition(pitch){
-      pitch = (pitch - ck.minPitch);
-      var r = +ck.rMin + (pitch * ck.octaveHeight / 12);
-      var theta = (1 - (ck.a * pitch)) * twelveth * pitch;
+      var twelveth = 2 * Math.PI / 12;
 
-      return cartesianCoordinates(r,theta);
-    }
-    $scope.getPosition = getPosition;
+      function display(pitch) {
+        return (pitch >= +ck.minPitch);
+      }
 
-    $scope.playNote = function (pitch) {
-      var p = instruPlayer.playNote({
-        pitch: pitch,
-        duration: 0.6
-      },true);
-      //p.on.then(function (data) {
-      //  $log.debug("triggered at : ",p.triggeredAt,", elapsed : ", data.elapsed);
-      //});
-    }
-  }]);
+      $scope.display = display;
+      function getPosition(pitch) {
+        pitch = (pitch - ck.minPitch);
+        var r = +ck.rMin + (pitch * ck.octaveHeight / 12);
+        var theta = (1 - (ck.a * pitch)) * twelveth * pitch;
+
+        return cartesianCoordinates(r, theta);
+      }
+
+      $scope.getPosition = getPosition;
+
+
+      var pitchesPlaying = [];
+
+      function isPlaying(pitch) {
+        return !!pitchesPlaying[pitch];
+      }
+      $scope.isPlaying = isPlaying;
+
+      $scope.playNote = function (pitch) {
+        var p = instruPlayer.playNote({
+          pitch: pitch,
+          duration: 0.6
+        }, true);
+        p.on.then(function (data) {
+          if(!pitchesPlaying[pitch]){
+            pitchesPlaying[pitch] = 0;
+          }
+          pitchesPlaying[pitch] += 1;
+        });
+        p.off.then(function (data) {
+          pitchesPlaying[pitch] -= 1;
+        });
+        //p.on.then(function (data) {
+        //  $log.debug("triggered at : ",p.triggeredAt,", elapsed : ", data.elapsed);
+        //});
+      }
+    }]);
