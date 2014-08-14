da77b3ad138826c36423f25a91d517697da3c4a5
Valentin Waeselynck <val@bandsquare.fr>
2014-08-15 00:08:08 +0200

    modal for waiting for sound fonts

---
+++ app/scripts/controllers/ModalCtrl.js
@@ -0,0 +1,20 @@
+angular.module('duodesicalApp')
+  .controller('ModalCtrl', ['$scope', 'instruPlayer', '$log', '$timeout', function ($scope, instruPlayer, $log, $timeout) {
+
+    $scope.fontsLoadingState = 'loading';
+
+    instruPlayer.loadedMidi
+      .then(function () {
+        $scope.fontsLoadingState = 'loaded';
+
+        var fadeAfter = 300;
+        $timeout(function () {
+          $scope.$close($scope.fontsLoadingState);
+        }, fadeAfter);
+
+      }, function (err) {
+        $log.error("Failed to load fonts : ", err);
+        $scope.fontsLoadingState = 'failed';
+      });
+
+  }]);
